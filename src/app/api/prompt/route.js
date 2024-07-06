5e6c462e5c0a324bfceee299dcbe5801be0bfcef
Aritra-Dey-117-XT <ece22127@iiitkalyani.ac.in>
2024-07-06 09:37:35 +0530

    Fixing 8: posts not showing again

--- src/app/api/prompt/route.js
+++ src/app/api/prompt/route.js
@@ -2,7 +2,7 @@ import { connectToDB } from "@/utils/database";
 import Prompt from "@/models/prompt";
 import { NextResponse } from "next/server"
 
-export async function GET() {
+export async function GET(request) {
     try {
         
         await connectToDB()
