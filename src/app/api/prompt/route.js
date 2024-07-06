14a10ce75e8f389fd896ab4bf394ee00be17e23e
Aritra-Dey-117-XT <ece22127@iiitkalyani.ac.in>
2024-07-06 09:47:03 +0530

    Fixing 11: posts not showing again

--- src/app/api/prompt/route.js
+++ src/app/api/prompt/route.js
@@ -7,9 +7,12 @@ export async function GET(request) {
         
         await connectToDB()
 
-        const allPrompts = await Prompt.find({}).populate("creator")
-
-        return NextResponse.json({message: "All Prompts Fetched!", allPrompts}, {status: 200})
+        try {
+            const allPrompts = await Prompt.find({}).populate("creator")
+            return NextResponse.json({message: "User Posts Fetched Successfully!", allPrompts}, {status: 200})
+        } catch (error) {
+            return NextResponse.json({message: "No Users Found!"}, {status: 404})
+        }
 
     } catch (error) {
         return NextResponse.json({error: error.message}, {status: 500})
